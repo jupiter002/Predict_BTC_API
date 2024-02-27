e2863d31bbd823ab679d53816c6cedb12cae36be
jupiter002 <jik7194@naver.com>
2024-02-27 20:14:12 +0900

    240225_Fix_KJH

--- main.py
+++ main.py
@@ -40,13 +40,13 @@ async def read_root(ticker1: str, ticker2:str):
     pred_price1, real_price1, date1 = get_crypto_price(ticker1)     # 전달받은 가상화폐 ticker를 함수에 인자값으로 전달
     pred_price2, real_price2, date2 = get_crypto_price(ticker2)
 
-    return {"days":date1, "pred_price1":pred_price1, "real_price1":real_price1,                        
-            "pred_price2":pred_price2, "real_price2":real_price2}           # 일시와 예측 가격데이터를 spring서버로 전달
+
+    return [{"days":date1, "value":pred_price1}, {"days":date1, "value":real_price1},                        
+            {"days":date1, "value":pred_price2}, {"days":date1, "value":real_price2}]           # 일시와 예측 가격데이터를 spring서버로 전달
 
 @app.get("/realtimeChart/{ticker}")
 async def read_root(ticker: str):
 
-<<<<<<< HEAD
     pred_price, real_price, date = get_crypto_price(ticker)     # 전달받은 가상화폐 ticker를 함수에 인자값으로 전달
 
     return {"days":date, "pred_price":pred_price, "real_price":real_price}           # 일시와 예측 가격데이터를 spring서버로 전달
@@ -55,11 +55,9 @@ async def read_root(ticker: str):
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
-=======
-    pred_price, real_price, date = real_time_chart(ticker)     # 전달받은 가상화폐 ticker를 함수에 인자값으로 전달
+    # pred_price, real_price, date = real_time_chart(ticker)     # 전달받은 가상화폐 ticker를 함수에 인자값으로 전달
         
-    return {"days":date, "pred_price":pred_price, "real_price":real_price}   
->>>>>>> 8fcc9e1fe28ffecd4a2c8d27e5240c82da6a5541
+    # return {"days":date, "pred_price":pred_price, "real_price":real_price}   
 
 
 # AI API
@@ -67,11 +65,7 @@ async def read_root(ticker: str):
 
 
 def fitting_to_real_price(df):                          # 학습 데이터를 Fitting 시키는 사용자 함수
-<<<<<<< HEAD
-    
-=======
         
->>>>>>> 8fcc9e1fe28ffecd4a2c8d27e5240c82da6a5541
     m = prh(                                            
     changepoint_prior_scale = 0.3,
     growth="linear",
@@ -80,23 +74,19 @@ def fitting_to_real_price(df):                          # 학습 데이터를 Fi
 
     m.fit(df)                                           # 학습데이터 Fitting
 
-    future = m.make_future_dataframe(periods=240, freq='H')      # 예상 주기 설정
+    future = m.make_future_dataframe(periods=240)      # 예상 주기 설정
 
     forecast = m.predict(future)                        # 예측한 값을 forecast변수에 저장
 
-    forecast['yhat'] = forecast['yhat'].astype('float') # 숫자형식을 float로 변환
+    # forecast['yhat'] = forecast['yhat'].astype('float') # 숫자형식을 float로 변환
 
-    # forecast['ds'] = forecast['ds'].astype('str')     # 예측과 실제 가격 추세 그래프를 양쪽으로 나눠서 그릴 수 있음
+    forecast['ds'] = forecast['ds'].astype('str')     # 예측과 실제 가격 추세 그래프를 양쪽으로 나눠서 그릴 수 있음
     
     return forecast                                     # 예측값 반환
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8fcc9e1fe28ffecd4a2c8d27e5240c82da6a5541
 def get_crypto_price(ticker="BTC"):                   # 가상화폐의 가격을 가져오는 사용자 함수
 
-    df = pyupbit.get_ohlcv(f"KRW-{ticker}", count=4320, interval="minute60")     # 원화 단위의 가상화폐, 시간 단위는 분 단위, 현재 시점부터 2000분 전의 데이터를 요청
+    df = pyupbit.get_ohlcv(f"KRW-{ticker}", count=4320, interval="day")     # 원화 단위의 가상화폐, 시간 단위는 분 단위, 현재 시점부터 2000분 전의 데이터를 요청
     df['y'] = df['close']
     df['ds'] = df.index
 
